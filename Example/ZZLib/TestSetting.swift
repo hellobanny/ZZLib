04e1de83bca839ddcfd21ad10979ad33ae85b4aa
张忠 <zhangzhong@zhangzhongdeMacBook-Pro.local>
2017-05-04 16:49:58 +0800

    更新了些

---
+++ Example/ZZLib/TestSetting.swift
@@ -0,0 +1,101 @@
+//
+//  TestSetting.swift
+//  ZZLib
+//
+//  Created by 张忠 on 2017/5/3.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import ZZLib
+
+class TestSetting: UITableViewController {
+    
+    var setting:MySetting!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        setting = MySetting(startSec: 0, baseVC: self, appid: "1213321956", color: UIColor.red, appname: "XXX")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return self.setting.numberOfSettingSections()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.setting.numberOfRowsIn(section: section)
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return setting.cellFor(indexPath: indexPath)!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            setting.clickedAt(indexPath: indexPath, cell: cell)
+        }
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ZZLib/Classes/MySetting.swift
@@ -0,0 +1,203 @@
+//
+//  MySetting.swift
+//  Pods
+//
+//  Created by 张忠 on 2017/5/3.
+//
+//
+
+import UIKit
+import Appirater
+import MessageUI
+
+public class MySetting: NSObject,MFMailComposeViewControllerDelegate {
+    
+    var startSectionIndex:Int
+    var baseController:UIViewController
+    var appID:String
+    var appName:String
+    var color = UIColor.orange
+    
+    public init(startSec:Int, baseVC:UIViewController, appid:String, color:UIColor, appname:String?) {
+        startSectionIndex = startSec
+        baseController = baseVC
+        appID = appid
+        if let n = appname {
+            appName = n
+        }
+        else {
+            appName = ""
+            let obj: AnyObject? = Bundle.main.infoDictionary!["CFBundleName"] as AnyObject?
+            if let name = obj as? String {
+                appName = name
+            }
+        }
+        self.color = color
+        
+        Appirater.setAppId(appID)
+        Appirater.setDaysUntilPrompt(7)
+        Appirater.setUsesUntilPrompt(5)
+        Appirater.setSignificantEventsUntilPrompt(-1)
+        Appirater.setTimeBeforeReminding(3)
+        Appirater.setDebug(false)
+        Appirater.appLaunched(true)
+        
+        super.init()
+    }
+    
+    public func numberOfSettingSections() -> Int {
+        return 2
+    }
+    
+    public func numberOfRowsIn(section:Int) -> Int{
+        let v = section - startSectionIndex
+        if v == 0 {
+            return 2
+        }
+        else if v == 1 {
+            return 0//more apps
+        }
+        else {
+            return 0
+        }
+    }
+    
+    //得到Cell
+    public func cellFor(indexPath:IndexPath) -> UITableViewCell? {
+        let sec = indexPath.section - startSectionIndex
+        let row = indexPath.row
+        if sec == 0 {
+            let cell = UITableViewCell()
+            let names = ["Support " + appName,"Complain " + appName]
+            cell.textLabel?.text = names[row]
+            cell.textLabel?.textColor = color
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        }
+        else if sec == 1 {
+        
+        }
+        return nil
+        
+    }
+    
+    //点击事件
+    public func clickedAt(indexPath:IndexPath,cell:UITableViewCell) {
+        let sec = indexPath.section - startSectionIndex
+        let row = indexPath.row
+        if sec == 0 {
+            if row == 0 {
+                let alertController = UIAlertController(title: "Support " + appName, message: nil, preferredStyle: .actionSheet)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                }
+                alertController.addAction(cancelAction)
+                
+                let rate = UIAlertAction(title: "Give 5 star review", style: .default ) { (action) -> Void in
+                    Appirater.rateApp()
+                }
+                alertController.addAction(rate)
+                
+                let share = UIAlertAction(title: "Share app to friends", style: .default ) { (action) -> Void in
+                    self.shareApp()
+                }
+                
+                alertController.addAction(share)
+                
+                alertController.view.tintColor = color
+                alertController.popoverPresentationController?.sourceView = cell
+                alertController.popoverPresentationController?.sourceRect = cell.bounds
+                baseController.present(alertController, animated: true, completion: nil)
+            }
+            else {
+                let alertController = UIAlertController(title: "Complain " + appName, message: nil, preferredStyle: .actionSheet)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+                }
+                alertController.addAction(cancelAction)
+                
+                let rate = UIAlertAction(title: "Complain by email", style: .default ) { (action) -> Void in
+                    self.emailFeedback()
+                }
+                alertController.addAction(rate)
+                
+                let share = UIAlertAction(title: "Contact with weixin", style: .default ) { (action) -> Void in
+                    self.contactWithWeixin()
+                }
+                alertController.addAction(share)
+                
+                alertController.view.tintColor = color
+                alertController.popoverPresentationController?.sourceView = cell
+                alertController.popoverPresentationController?.sourceRect = cell.bounds
+                baseController.present(alertController, animated: true, completion: nil)
+            }
+
+        }
+        else if sec == 1 {
+        
+        }
+    }
+    
+    fileprivate func shareApp(){
+        let text = "\(appName) https://itunes.apple.com/app/id\(appID)"
+        let items = [text]
+        let activityVC = UIActivityViewController(activityItems: items, applicationActivities: nil)
+        activityVC.excludedActivityTypes = [UIActivityType.assignToContact]
+        activityVC.popoverPresentationController?.sourceView = baseController.view
+        activityVC.popoverPresentationController?.sourceRect = baseController.view.bounds
+        baseController.present(activityVC, animated: true, completion: nil)
+    }
+    
+    fileprivate func contactWithWeixin() {
+        UIPasteboard.general.string = "hellobanny"
+        let av = UIAlertController(title: "Notice", message: "Add hellobanny in weixin and start a chat. The user name has add to your paste board.", preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        av.addAction(cancel)
+        let done = UIAlertAction(title: "Open Weixin", style: .default) { (_) in
+            if let url = URL(string: "weixin://") {
+                if #available(iOS 10.0, *) {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                } else {
+                    // Fallback on earlier versions
+                    UIApplication.shared.openURL(url)
+                }
+            }
+        }
+        av.addAction(done)
+        baseController.present(av, animated: true, completion: nil)
+        
+    }
+    
+    fileprivate func emailFeedback(){
+        if MFMailComposeViewController.canSendMail(){
+            let mvc = MFMailComposeViewController()
+            mvc.mailComposeDelegate = self
+            mvc.setSubject("Feedback")
+            mvc.setToRecipients(["hellobanny@gmail.com"])
+            var version = ""
+            let vobj: AnyObject? = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as AnyObject?
+            if let ver = vobj as? String{
+                version = ver
+            }
+            let device = UIDevice.current
+            let size = UIScreen.main.bounds.size
+            mvc.setMessageBody("\n\n\n-----------------------------\n Name: \(appName)\n Device: \(device.localizedModel) \n OS Version:\(device.systemVersion)\n App Version:\(version)\n Screen:\(size.width)*\(size.height)\n", isHTML: false)
+            mvc.popoverPresentationController?.sourceView = baseController.view
+            mvc.popoverPresentationController?.sourceRect = baseController.view.bounds
+            baseController.present(mvc, animated: true, completion: nil)
+        }
+        else {
+            let alertController = UIAlertController(title: "Notice", message: "Your device doesn't support send email.", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: NSLocalizedString("OK",comment:""), style: .cancel) { (action) in
+                print(action)
+            }
+            alertController.addAction(cancelAction)
+            baseController.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    public func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?){
+        controller.dismiss(animated: true, completion: nil)
+    }
+}
+
